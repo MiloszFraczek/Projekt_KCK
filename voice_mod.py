2053d13498b85771a5af7f29b39fa88c800eb72b
C7B3RR <cyberrox509@gmail.com>
2026-06-08 16:57:30 +0200

    ujednolicenie komunikatów błędu w trakcie wykonywania ćwiczenia

---
+++ voice_mod.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr
+import pyttsx3
+from enum import Enum
+class VoiceMod:
+    def __init__(self):
+        pass
+    class Mode(Enum):
+        start = 1
+        stop = 2
+
+    def speak(self,tekst):
+        silnik = pyttsx3.init()
+        silnik.say(tekst)
+        silnik.runAndWait()
+
+    def get_voice(self):
+        recognizer = sr.Recognizer()
+
+        with sr.Microphone() as zrodlo:
+            print("Nasłuchuję... (powiedz coś)")
+            recognizer.adjust_for_ambient_noise(zrodlo)
+            audio = recognizer.listen(zrodlo)
+
+            try:
+                tekst = recognizer.recognize_google(audio, language="pl-PL")
+                print(f"Rozpoznano: {tekst}")
+                return tekst
+            except sr.UnknownValueError:
+                print("Przepraszam, nie zrozumiałem co powiedziałeś.")
+                return None
+            except sr.RequestError as e:
+                print(f"Błąd połączenia z serwisem Google: {e}")
+                return None
+    def change_state(self):
+        voice = self.get_voice()
+        if voice == "rozpocznij trening":
+            return VoiceMod.Mode.start
+    def mistake_tell(self, mistake):
+        match mistake:
+            case "legs_width":
+                self.speak("Nogi za wąsko")
+            case "straight_back":
+                self.speak("Wyprostuj plecy")
+            case "delts_asymetry":
+                self.speak("Popraw barki")
+            case "bent_arms":
+                self.speak("Wyprostuj łokcie")
+            case "hips_too_low":
+                self.speak("Biodra za nisko")
+            case "hips_too_high":
+                self.speak("Biodra za wysoko")
