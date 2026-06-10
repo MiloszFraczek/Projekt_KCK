7369cb10e85c366870051074bae04f541bf5c558
C7B3RR <cyberrox509@gmail.com>
2026-06-10 14:56:37 +0200

    Refraktoryzacja kodu

--- voice_mod.py
+++ voice_mod.py
@@ -1,30 +1,26 @@
 import speech_recognition as sr
 import pyttsx3
-from enum import Enum
+
 class VoiceMod:
     def __init__(self):
         pass
-    class Mode(Enum):
-        start = 1
-        stop = 2
-
-    def speak(self,tekst):
-        silnik = pyttsx3.init()
-        silnik.say(tekst)
-        silnik.runAndWait()
+    def speak(self,text):
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
 
     def get_voice(self):
         recognizer = sr.Recognizer()
 
-        with sr.Microphone() as zrodlo:
+        with sr.Microphone() as source:
             print("Nasłuchuję... (powiedz coś)")
-            recognizer.adjust_for_ambient_noise(zrodlo)
-            audio = recognizer.listen(zrodlo)
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
 
             try:
-                tekst = recognizer.recognize_google(audio, language="pl-PL")
-                print(f"Rozpoznano: {tekst}")
-                return tekst
+                text = recognizer.recognize_google(audio, language="pl-PL")
+                print(f"Rozpoznano: {text}")
+                return text
             except sr.UnknownValueError:
                 print("Przepraszam, nie zrozumiałem co powiedziałeś.")
                 return None
@@ -37,11 +33,15 @@ class VoiceMod:
             return VoiceMod.Mode.start
     def mistake_tell(self, mistake):
         match mistake:
-            case legs_width:
+            case "legs_width":
                 self.speak("Nogi za wąsko")
-            case straight_back:
+            case "straight_back":
                 self.speak("Wyprostuj plecy")
-            case delts_asymetry:
+            case "delts_asymetry":
                 self.speak("Popraw barki")
-            case too_fast:
-                self.speak("Za szybko opuszczasz ciężar")
+            case "bent_arms":
+                self.speak("Wyprostuj łokcie")
+            case "hips_too_low":
+                self.speak("Biodra za nisko")
+            case "hips_too_high":
+                self.speak("Biodra za wysoko")
